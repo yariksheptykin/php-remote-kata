89aa0ed625a2d32fe1b94206e7403b8ecc58bff7
Iaroslav Sheptykin <i.sheptykin@neusta.de>
2024-03-07 12:18:58 +0100

    "mob next [ci-skip]"

---
+++ tests/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Kata\Tests;
+
+use SetupTest;
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    public function testSetup()
+    {
+        self::assertTrue(false);
+    }
+}
