799bb0fe5df75693e1da59c81a78985195be32d5
i.csakany <i.csakany@neusta.de>
2024-04-10 16:18:56 +0200

    "mob next [ci-skip]"

---
+++ tests/CircularBufferTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kata\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class CircularBufferTest extends TestCase
+{
+    private $circularBuffer;
+
+    public function testCircularBufferIsCreated() {
+        $buffersize = 3;
+        $this->circularBuffer = new \Kata\CircularBuffer( $buffersize);
+        self::assertSame($buffersize, $this->circularBuffer->getSize());
+
+    }
+
+}
