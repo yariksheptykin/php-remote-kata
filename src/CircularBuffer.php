0e4c96ce0079ff50302535407eee867d7e07c2f4
ttanase <t.tanase@neusta.de>
2024-04-11 12:01:33 +0200

    "mob next [ci-skip]"

--- src/CircularBuffer.php
+++ src/CircularBuffer.php
@@ -6,18 +6,27 @@ class CircularBuffer
 {
 
     private array $buffer = [];
-    
-    public function __construct(int $size)
+
+    public function __construct(private readonly int $size)
     {
     }
 
     public function add(int $element): void
     {
+        if (count($this->buffer) === $this->size) {
+            $this->take();
+        }
+        
         $this->buffer[] = $element;
     }
 
     public function take(): int
     {
         return array_shift($this->buffer);
+    }
+
+    public function count(): int
+    {
+        return count($this->buffer);
     }
 }
--- tests/CircularBufferTest.php
+++ tests/CircularBufferTest.php
@@ -7,23 +7,39 @@ use PHPUnit\Framework\TestCase;
 
 class CircularBufferTest extends TestCase
 {
+    private CircularBuffer $circularBuffer;
+
+    public function setUp(): void
+    {
+        $this->circularBuffer = new CircularBuffer(3);
+        
+        parent::setUp();
+    }
+
     public function testAddMultipleElementsToBuffer(): void
     {
-        $circularBuffer = new CircularBuffer(3);
-        $circularBuffer->add(2);
-        $circularBuffer->add(3);
+        $this->circularBuffer->add(2);
+        $this->circularBuffer->add(3);
         
-        self::assertSame(2, $circularBuffer->take());
-        self::assertSame(3, $circularBuffer->take());
+        self::assertSame(2, $this->circularBuffer->take());
+        self::assertSame(3, $this->circularBuffer->take());
     }
     
     public function testCountMultipleElementsToBuffer(): void
     {
-        $circularBuffer = new CircularBuffer(3);
-        $circularBuffer->add(2);
-        $circularBuffer->add(3);
+        $this->circularBuffer->add(2);
+        $this->circularBuffer->add(3);
 
-        self::markTestIncomplete('to be done by teo');
-        self::assertSame(2, $circularBuffer->count());
+        self::assertSame(2, $this->circularBuffer->count());
+    }
+    
+    public function testAddMoreElementsThanCapacityOfBuffer(): void
+    {
+        $this->circularBuffer->add(1);
+        $this->circularBuffer->add(2);
+        $this->circularBuffer->add(3);
+        $this->circularBuffer->add(4);
+        
+        self::assertSame(2, $this->circularBuffer->take());
     }
 }
